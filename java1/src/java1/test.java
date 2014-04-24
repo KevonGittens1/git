fbe25d47b69cbede55187345a341d0c1089eb3a4
Kevon Gittens <kevon@pollardsetfilles.com>
2014-04-24 11:46:52 -0400

    updated the gitit and tested the code

--- java1/src/java1/test.java
+++ java1/src/java1/test.java
@@ -3,13 +3,14 @@ package java1;
 public class test {
   /*this is a test of git hub push and pull stuff*/
 	
-	public void main()
+	public static void main()
 	{
 		gitit();
 	}
 	
-	public void gitit()
+	public static void gitit() //get it lol
 	{
 		System.out.println("not bad git not bad at all");
+		System.out.println("some more to see how the git stuff really works");
 	}
 }

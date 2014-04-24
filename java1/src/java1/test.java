47da7a313986efc8c01f1ce6a2249e5767fbbccc
Kevon Gittens <kevon@pollardsetfilles.com>
2014-04-24 11:30:40 -0400

    first push

---
+++ java1/src/java1/test.java
@@ -0,0 +1,16 @@
+package java1;
+
+public class test {
+  /*this is a test of git hub push and pull stuff*/
+	
+	public void main()
+	{
+		gitit();
+	}
+	
+	public void gitit()
+	{
+		System.out.println("not bad git not bad at all");
+	}
+	
+}
